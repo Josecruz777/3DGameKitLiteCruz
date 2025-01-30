6d6afb9bb798fe32fb48576fe0d2265c3d013114
Josecruz777 <756199@student.dusd.net>
2025-01-30 15:23:54 -0800

    added stuff
    added acid inside of hills and added a moving platform

--- 3DGameKitLite/Assets/NavMeshAgent.cs
+++ 3DGameKitLite/Assets/NavMeshAgent.cs
@@ -1,9 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NavMeshAgent : MonoBehaviour
 {
+    internal NavMeshPathStatus pathStatus;
+    internal bool updatePosition;
+    internal float angularSpeed;
+    internal bool isOnNavMesh;
+    internal Vector3 nextPosition;
+    internal float speed;
+
+    internal void ResetPath()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal bool SetDestination(Vector3 position)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void Warp(Vector3 position)
+    {
+        throw new NotImplementedException();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
